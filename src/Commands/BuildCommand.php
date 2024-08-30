19b0cecfb7feaedd74d5fff1259db8d5669f1f79
Simon Hamp <simon.hamp@me.com>
2024-08-30 12:38:38 +0100

    Alt implementation

--- src/Commands/BuildCommand.php
+++ src/Commands/BuildCommand.php
@@ -7,12 +7,14 @@ use Illuminate\Support\Facades\Process;
 use Illuminate\Support\Str;
 use Native\Electron\Concerns\LocatesPhpBinary;
 use Native\Electron\Facades\Updater;
+use Native\Electron\Traits\InstallsAppIcon;
 use Native\Electron\Traits\OsAndArch;
 
 class BuildCommand extends Command
 {
     use LocatesPhpBinary;
     use OsAndArch;
+    use InstallsAppIcon;
 
     protected $signature = 'native:build
         {os? : The operating system to build for (all, linux, mac, win)}
@@ -40,13 +42,15 @@ class BuildCommand extends Command
         // Added checks for correct input for os and arch
         $os = $this->selectOs($this->argument('os'));
 
+        $this->installIcon();
+
         $buildCommand = 'build';
         if ($os != 'all') {
             $arch = $this->selectArchitectureForOs($os, $this->argument('arch'));
 
             $os .= $arch != 'all' ? "-{$arch}" : '';
 
-            // Wether to publish the app or not
+            // Should we publish?
             if ($publish = ($this->option('publish'))) {
                 $buildCommand = 'publish';
             }
