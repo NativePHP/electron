2646655f0f7a72d184f116751585da6904aa0b4e
Simon Hamp <simon.hamp@me.com>
2024-09-05 01:58:52 +0100

    Copy the installer icon

--- src/Traits/InstallsAppIcon.php
+++ src/Traits/InstallsAppIcon.php
@@ -14,6 +14,7 @@ trait InstallsAppIcon
         @copy(public_path('icon.png'), __DIR__.'/../../resources/js/resources/icon.png');
         @copy(public_path('IconTemplate.png'), __DIR__.'/../../resources/js/resources/IconTemplate.png');
         @copy(public_path('IconTemplate@2x.png'), __DIR__.'/../../resources/js/resources/IconTemplate@2x.png');
+        @copy(public_path('icon.png'), __DIR__.'/../../resources/js/build/icon.png');
 
         note('App icons copied');
     }
