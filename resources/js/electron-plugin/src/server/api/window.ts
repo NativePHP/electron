c75d1e6877f0cf756a7d7248f979aa62d86cd8c3
Simon Hamp <simon.hamp@me.com>
2024-10-24 16:01:34 +0100

    code style

--- resources/js/electron-plugin/src/server/api/window.ts
+++ resources/js/electron-plugin/src/server/api/window.ts
@@ -1,67 +1,95 @@
-import express from 'express'
-import {BrowserWindow, clipboard, NativeImage} from 'electron'
-import state from '../state'
+import express from 'express';
+import {BrowserWindow, clipboard, NativeImage} from 'electron';
+import state from '../state';
 import {join} from "path";
 import {notifyLaravel} from "../utils";
 const router = express.Router();
 import windowStateKeeper from "electron-window-state";
 
 router.post('/maximize', (req, res) => {
-    const {id} = req.body
-    state.windows[id]?.maximize()
+    const {id} = req.body;
 
-    res.sendStatus(200)
+    state.windows[id]?.maximize();
+
+    res.sendStatus(200);
 });
 
 router.post('/minimize', (req, res) => {
-  const {id} = req.body
-  state.windows[id]?.minimize()
+    const {id} = req.body;
 
-  res.sendStatus(200)
+    state.windows[id]?.minimize();
+
+    res.sendStatus(200);
 });
 
 router.post('/resize', (req, res) => {
-    const {id, width, height} = req.body
+    const {id, width, height} = req.body;
 
-    state.windows[id]?.setSize(parseInt(width), parseInt(height))
+    state.windows[id]?.setSize(parseInt(width), parseInt(height));
 
-    res.sendStatus(200)
-})
+    res.sendStatus(200);
+});
 
+router.post('/title', (req, res) => {
+    const {id, title} = req.body;
+
+    state.windows[id]?.setTitle(title);
+
+    res.sendStatus(200);
+});
+
+router.post('/url', (req, res) => {
+    const {id, url} = req.body;
+
+    state.windows[id]?.loadURL(url);
+
+    res.sendStatus(200);
+});
 
 router.post('/position', (req, res) => {
-  const {id, x, y, animate} = req.body
+    const {id, x, y, animate} = req.body;
 
-  state.windows[id]?.setPosition(parseInt(x), parseInt(y), animate)
+    state.windows[id]?.setPosition(parseInt(x), parseInt(y), animate);
 
-  res.sendStatus(200)
-})
+    res.sendStatus(200);
+});
 
 router.post('/reload', (req, res) => {
-  const {id} = req.body
-  state.windows[id]?.reload()
+    const {id} = req.body;
 
-  res.sendStatus(200)
+    state.windows[id]?.reload();
+
+    res.sendStatus(200);
 });
 
 router.post('/close', (req, res) => {
-    const {id} = req.body
+    const {id} = req.body;
 
     if (state.windows[id]) {
-        state.windows[id].close()
-        delete state.windows[id]
+        state.windows[id].close();
+        delete state.windows[id];
     }
-    return res.sendStatus(200)
-})
+
+    return res.sendStatus(200);
+});
 
 router.post('/hide', (req, res) => {
-    const {id} = req.body
+    const {id} = req.body;
 
     if (state.windows[id]) {
-        state.windows[id].hide()
+        state.windows[id].hide();
     }
-    return res.sendStatus(200)
-})
+
+    return res.sendStatus(200);
+});
+
+router.post('/always-on-top', (req, res) => {
+    const {id, alwaysOnTop} = req.body;
+
+    state.windows[id]?.setAlwaysOnTop(alwaysOnTop);
+
+    res.sendStatus(200);
+});
 
 router.get('/current', (req, res) => {
     // Find the current window object
@@ -118,14 +146,6 @@ function getWindowData(id) {
         // trafficLightPosition: currentWindow.getTrafficLightPosition(),
     };
 }
-});
-
-router.post('/always-on-top', (req, res) => {
-    const {id, alwaysOnTop} = req.body
-    state.windows[id]?.setAlwaysOnTop(alwaysOnTop)
-
-    res.sendStatus(200)
-});
 
 router.post('/open', (req, res) => {
     let {
@@ -159,24 +179,24 @@ router.post('/open', (req, res) => {
         fullscreenable,
         kiosk,
         autoHideMenuBar,
-    } = req.body
+    } = req.body;
 
     if (state.windows[id]) {
-        state.windows[id].show()
-        state.windows[id].focus()
-        return res.sendStatus(200)
+        state.windows[id].show();
+        state.windows[id].focus();
+        return res.sendStatus(200);
     }
 
     let preloadPath = join(__dirname, '../../electron-plugin/dist/preload/index.js')
 
-    let windowState: windowStateKeeper.State | undefined = undefined
+    let windowState: windowStateKeeper.State | undefined = undefined;
 
     if (req.body.rememberState === true) {
-      windowState = windowStateKeeper({
-        file: `window-state-${id}.json`,
-        defaultHeight: parseInt(height),
-        defaultWidth: parseInt(width),
-      })
+        windowState = windowStateKeeper({
+            file: `window-state-${id}.json`,
+            defaultHeight: parseInt(height),
+            defaultWidth: parseInt(width),
+        });
     }
 
     const window = new BrowserWindow({
@@ -217,93 +237,95 @@ router.post('/open', (req, res) => {
         fullscreen,
         fullscreenable,
         kiosk,
-    })
+    });
 
     if ((process.env.NODE_ENV === 'development' || showDevTools === true) && showDevTools !== false) {
         window.webContents.openDevTools();
     }
 
-    require("@electron/remote/main").enable(window.webContents)
+    require("@electron/remote/main").enable(window.webContents);
 
     if (req.body.rememberState === true) {
-        windowState.manage(window)
+        windowState.manage(window);
     }
 
     window.on('blur', () => {
         notifyLaravel('events', {
-          event: 'Native\\Laravel\\Events\\Windows\\WindowBlurred',
-          payload: [id]
-        })
+            event: 'Native\\Laravel\\Events\\Windows\\WindowBlurred',
+            payload: [id]
+        });
     });
 
     window.on('focus', () => {
         notifyLaravel('events', {
             event: 'Native\\Laravel\\Events\\Windows\\WindowFocused',
             payload: [id]
-        })
-    })
+        });
+    });
 
     window.on('minimize', () => {
         notifyLaravel('events', {
             event: 'Native\\Laravel\\Events\\Windows\\WindowMinimized',
             payload: [id]
-        })
-    })
+        });
+    });
 
     window.on('maximize', () => {
-      notifyLaravel('events', {
-        event: 'Native\\Laravel\\Events\\Windows\\WindowMaximized',
-        payload: [id]
-      })
-    })
+        notifyLaravel('events', {
+            event: 'Native\\Laravel\\Events\\Windows\\WindowMaximized',
+            payload: [id]
+        });
+    });
 
-  window.on('show', () => {
-    notifyLaravel('events', {
-      event: 'Native\\Laravel\\Events\\Windows\\WindowShown',
-      payload: [id]
-    })
-  });
+    window.on('show', () => {
+        notifyLaravel('events', {
+            event: 'Native\\Laravel\\Events\\Windows\\WindowShown',
+            payload: [id]
+        });
+    });
 
     window.on('resized', () => {
-      notifyLaravel('events', {
-        event: 'Native\\Laravel\\Events\\Windows\\WindowResized',
-        payload: [id, window.getSize()[0], window.getSize()[1]]
-      })
+        notifyLaravel('events', {
+            event: 'Native\\Laravel\\Events\\Windows\\WindowResized',
+            payload: [id, window.getSize()[0], window.getSize()[1]]
+        });
     });
 
     window.on('page-title-updated', (evt) => {
-        evt.preventDefault()
-    })
+        evt.preventDefault();
+    });
 
     window.on('close', (evt) => {
         if (state.windows[id]) {
-            delete state.windows[id]
+            delete state.windows[id];
         }
+
         notifyLaravel('events', {
             event: 'Native\\Laravel\\Events\\Windows\\WindowClosed',
             payload: [id]
-        })
-    })
+        });
+    });
 
     // @ts-ignore
     window.on('hide', (evt) => {
         notifyLaravel('events', {
             event: 'Native\\Laravel\\Events\\Windows\\WindowHidden',
             payload: [id]
-        })
-    })
+        });
+    });
 
     // Append the window id to the url
-    url += (url.indexOf('?') === -1 ? '?' : '&') + '_windowId=' + id
+    url += (url.indexOf('?') === -1 ? '?' : '&') + '_windowId=' + id;
 
-    window.loadURL(url)
+    window.loadURL(url);
 
     window.webContents.on('did-finish-load', () => {
-        window.show()
-    })
-    state.windows[id] = window
+        window.show();
+    });
 
-    res.sendStatus(200)
-})
+    state.windows[id] = window;
+
+    res.sendStatus(200);
+});
 
 export default router;
