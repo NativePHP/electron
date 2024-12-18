5fce6d135befed42d14e04d4c3df54bd7055f5d8
JustinLawrenceMS <justin.lawrence.codes@gmail.com>
2024-12-18 17:13:29 -0600

    test - check logic upstream 1

--- tests/InitializationTest.php
+++ tests/InitializationTest.php
@@ -1,40 +1,43 @@
 <?php
 
 use PHPUnit\Framework\ExpectationFailedException;
-
 use function Orchestra\Testbench\remote;
 
-it('can boot up the app', function () {
-    $output = '';
+test('critical feature', function () {
+    stopIfConditionFails(!is_null('This is not null.'), 'Required condition is missing. Stopping tests.');
 
-    $process = remote('native:serve --no-dependencies --no-interaction');
-    $process->start(function ($type, $line) use (&$output) {
-        $output .= $line;
+    it('can boot up the app', function () {
+        $output = '';
+
+        $process = remote('native:serve --no-dependencies --no-interaction');
+        $process->start(function ($type, $line) use (&$output) {
+            $output .= $line;
+        });
+
+        try {
+            retry(20, function () use ($output) {
+                // Wait until port 8100 is open
+                dump('Waiting for port 8100 to open...');
+
+                $fp = @fsockopen('localhost', 8100, $errno, $errstr, 1);
+                if ($fp === false) {
+                    throw new Exception(sprintf(
+                        'Port 8100 is not open yet. Output: "%s"',
+                        $output,
+                    ));
+                }
+            }, 5000);
+        } finally {
+            $process->stop();
+        }
+
+        try {
+            expect($output)->toContain('Running the dev script with npm');
+        } catch (ExpectationFailedException) {
+            throw new ExpectationFailedException(sprintf(
+                '"%s" does not match the expected output.',
+                $output,
+            ));
+        }
     });
-
-    try {
-        retry(20, function () use ($output) {
-            // Wait until port 8100 is open
-            dump('Waiting for port 8100 to open...');
-
-            $fp = @fsockopen('localhost', 8100, $errno, $errstr, 1);
-            if ($fp === false) {
-                throw new Exception(sprintf(
-                    'Port 8100 is not open yet. Output: "%s"',
-                    $output,
-                ));
-            }
-        }, 5000);
-    } finally {
-        $process->stop();
-    }
-
-    try {
-        expect($output)->toContain('Running the dev script with npm');
-    } catch (ExpectationFailedException) {
-        throw new ExpectationFailedException(sprintf(
-            '"%s" does not match the expected output.',
-            $output,
-        ));
-    }
 });
--- tests/Pest.php
+++ tests/Pest.php
@@ -3,3 +3,11 @@
 use Native\Electron\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
+
+function stopIfConditionFails(bool $condition, string $message): void
+{
+    if (!$condition) {
+        test()->markTestSkipped($message);
+        exit(1); // Stop all remaining tests
+    }
+}
