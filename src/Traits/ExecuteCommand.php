68375f4eeece24fa25e3a166e9d58ed6ce72933e
Eser DENIZ <srwiez@gmail.com>
2024-12-22 16:34:16 +0100

    fix: cleaning

--- src/Traits/ExecuteCommand.php
+++ src/Traits/ExecuteCommand.php
@@ -35,8 +35,6 @@ trait ExecuteCommand
 
         note('Fetching latest dependencies…');
 
-        $this->info(var_export($envs[$type], true));
-
         Process::path(__DIR__.'/../../resources/js/')
             ->env($envs[$type])
             ->forever()
