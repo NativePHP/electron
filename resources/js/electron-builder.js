2f6446f0893b91b5b1567ff9a7b08dd33406d6f4
gwleuverink <willem@leuver.ink>
2025-01-08 10:09:18 +0100

    remove redundant electron-builder app copy logic

--- resources/js/electron-builder.js
+++ resources/js/electron-builder.js
@@ -39,6 +39,7 @@ let updaterConfig = {};
 // We wouldn't need these since its not representing the target platform
 console.log("Arch: ", process.arch)
 console.log("Platform: ", process.platform)
+
 try {
     updaterConfig = process.env.NATIVEPHP_UPDATER_CONFIG;
     updaterConfig = JSON.parse(updaterConfig);
@@ -47,7 +48,6 @@ try {
 }
 
 if (isBuilding) {
-
     console.log();
     console.log('===================================================================');
     console.log('                    Building for ' + targetOs);
@@ -55,74 +55,6 @@ if (isBuilding) {
     console.log();
     console.log('Updater config', updaterConfig);
     console.log();
-
-    try {
-        const appPath = join(import.meta.dirname, 'resources', 'app');
-
-        removeSync(appPath);
-
-        // As we can't copy into a subdirectory of ourself we need to copy to a temp directory
-        let tmpDir = mkdtempSync(join(os.tmpdir(), 'nativephp'));
-
-        copySync(process.env.APP_PATH, tmpDir, {
-            overwrite: true,
-            dereference: true,
-            filter: (src, dest) => {
-                let skip = [
-                    // Skip .git and Dev directories
-                    join(process.env.APP_PATH, '.git'),
-                    join(process.env.APP_PATH, 'docker'),
-                    join(process.env.APP_PATH, 'packages'),
-
-                    // Only needed for local testing
-                    join(process.env.APP_PATH, 'vendor', 'nativephp', 'electron', 'vendor'),
-                    join(process.env.APP_PATH, 'vendor', 'nativephp', 'laravel', 'vendor'),
-
-                    join(process.env.APP_PATH, 'vendor', 'nativephp', 'php-bin'),
-                    join(process.env.APP_PATH, 'vendor', 'nativephp', 'electron', 'bin'),
-                    join(process.env.APP_PATH, 'vendor', 'nativephp', 'electron', 'resources'),
-                    join(process.env.APP_PATH, 'node_modules'),
-                    join(process.env.APP_PATH, 'dist'),
-                ];
-
-                let shouldSkip = false;
-                skip.forEach((path) => {
-                    if (src.indexOf(path) === 0) {
-                        shouldSkip = true;
-                    }
-                });
-
-                return !shouldSkip;
-            }
-        });
-
-        copySync(tmpDir, appPath);
-
-        // Electron build removes empty folders, so we have to create dummy files
-        // dotfiles unfortunately don't work.
-        writeJsonSync(join(appPath, 'storage', 'framework', 'cache', '_native.json'), {})
-        writeJsonSync(join(appPath, 'storage', 'framework', 'sessions', '_native.json'), {})
-        writeJsonSync(join(appPath, 'storage', 'framework', 'testing', '_native.json'), {})
-        writeJsonSync(join(appPath, 'storage', 'framework', 'views', '_native.json'), {})
-        writeJsonSync(join(appPath, 'storage', 'app', 'public', '_native.json'), {})
-        writeJsonSync(join(appPath, 'storage', 'logs', '_native.json'), {})
-
-        removeSync(tmpDir);
-
-        console.log();
-        console.log('Copied app to resources');
-        console.log(join(process.env.APP_PATH, 'dist'));
-        console.log();
-        console.log('===================================================================');
-        console.log('                       Starting build...');
-        console.log();
-    } catch (e) {
-        console.error();
-        console.error('Error copying app into build environment');
-        console.error(e);
-        console.error();
-    }
-
 }
 
 export default {
