66af0b0a04fccd2ff85629c4382cd65f0d1dac27
Pete Bishop <pbishop@monarx.com>
2025-02-18 14:12:25 +0000

    Allow dynamic properties on mock class

--- tests/Mocks/HasPreAndPostProcessingMock.php
+++ tests/Mocks/HasPreAndPostProcessingMock.php
@@ -5,6 +5,7 @@ namespace Native\Electron\Tests\Mocks;
 use Illuminate\Console\Command;
 use Native\Electron\Traits\HasPreAndPostProcessing;
 
+#[\AllowDynamicProperties]
 class HasPreAndPostProcessingMock extends Command
 {
     use HasPreAndPostProcessing;
