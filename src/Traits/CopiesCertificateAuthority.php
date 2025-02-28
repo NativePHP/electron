da6b5c3fb407790c858b8cc4b8d79c260e650877
Pete Bishop <pbishop@monarx.com>
2025-02-28 17:53:34 +0000

    Test for new trait

--- src/Traits/CopiesCertificateAuthority.php
+++ src/Traits/CopiesCertificateAuthority.php
@@ -5,6 +5,7 @@ namespace Native\Electron\Traits;
 use Symfony\Component\Filesystem\Path;
 use function Laravel\Prompts\error;
 use function Laravel\Prompts\intro;
+use function Laravel\Prompts\warning;
 
 trait CopiesCertificateAuthority
 {
@@ -13,9 +14,26 @@ trait CopiesCertificateAuthority
         try {
             intro('Copying latest CA Certificate...');
 
+            $phpBinaryDirectory = base_path('vendor/nativephp/php-bin/');
+
+            // Check if the class this trait is used in also uses LocatesPhpBinary
+            if (method_exists($this, 'phpBinaryPath')) {
+                // Get binary directory but up one level
+                $phpBinaryDirectory = dirname(base_path($this->phpBinaryPath()));
+            }
+
+            $certificateFileName = 'cacert.pem';
+            $certFilePath = Path::join($phpBinaryDirectory, $certificateFileName);
+
+            if (!file_exists($certFilePath)) {
+                warning('CA Certificate not found at ' . $certFilePath . '. Skipping copy.');
+
+                return;
+            }
+
             $copied = copy(
-                Path::join($this->sourcePath(), 'vendor', 'nativephp', 'php-bin', 'cacert.pem'),
-                Path::join($this->sourcePath(), 'vendor', 'nativephp', 'electron', 'resources', 'js', 'resources', 'cacert.pem')
+                $certFilePath,
+                Path::join(base_path('vendor/nativephp/electron/resources/js/resources'), $certificateFileName)
             );
 
             if (!$copied) {
